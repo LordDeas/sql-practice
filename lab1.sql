e4548b793add346f5d5bbe0f4c6d7633cfe4b4d1
Andrii <cackemimi@gmail.com>
2025-02-01 11:09:59 +0200

    Data updated

--- lab1.sql
+++ lab1.sql
@@ -6,21 +6,21 @@ from customers c
 LEFT Join orders o on o.customer_id = c.customer_id
 Group by c.customer_id, c.first_name, c.last_name;
 --
--- клиенти які зробили за весь час <5000
+-- clients who have made <5000 in all time
 select c.first_name,c.last_name, sum(oi.quantity*oi.list_price) as total_sum
 from customers c
 inner join orders o on c.customer_id= o.customer_id
 Join order_items oi on o.order_id = oi.order_id
 Group by c.first_name,c.last_name
 having sum(oi.quantity*oi.list_price) > 5000;
---сума останього замовлення()
+--last order amount()
 with sort_order as ( select c.first_name,c.last_name, o.order_id, o.order_date,  
 row_number() over(partition by c.customer_id order by o.order_date desc ) as rn
 from customers c
 Join orders o on c.customer_id=o.customer_id
 
 )
---join order_items oi on o.order_id = oi.order_id
+--
 select so.first_name,so.last_name,so.order_date, sum(oi.quantity*oi.list_price)
 from sort_order so
 Join order_items oi on oi.order_id=so.order_id
